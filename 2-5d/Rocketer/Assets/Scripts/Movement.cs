5fa6fe91821608cb0f95d04c6b0de2823a931c9f
Daniel Boj <dboj@uoc.edu>
2024-11-01 15:28:54 +0100

    Update CommitL v0.0.1 -> Añadido movimiento básico al actor principal: Impulso y rotación.

--- 2-5d/Rocketer/Assets/Scripts/Movement.cs
+++ 2-5d/Rocketer/Assets/Scripts/Movement.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    Rigidbody rigidBody;
+
+    [SerializeField] float mainThrust = 1000f;
+    [SerializeField] float rotationSpeed = 100f;
+
+    int rotateForward = -1;
+    int rotateBackward = 1;
+
     void Start()
     {
-        
+
+        rigidBody = GetComponent<Rigidbody>();
+
     }
 
     // Update is called once per frame
@@ -18,7 +28,47 @@ public class Movement : MonoBehaviour
 
     }
 
-    private void ProcessInput() {
-        Input.GetKey
+    private void ProcessInput()
+    {
+
+        Thrust();
+        Rotation();
+
     }
+
+    private void Thrust()
+    {
+
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidBody.AddRelativeForce(Vector3.up * mainThrust * Time.deltaTime);
+        }
+    }
+
+    private void Rotation()
+    {
+
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            Rotate(rotateBackward);
+            return;
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            Rotate(rotateForward);
+            return;
+        }
+
+    }
+
+    private void Rotate(int directionMultiplier)
+    {
+
+        rigidBody.freezeRotation = true;
+        transform.Rotate(Vector3.forward * rotationSpeed * Time.deltaTime * directionMultiplier);
+        rigidBody.freezeRotation = false;
+
+    }
+
 }
